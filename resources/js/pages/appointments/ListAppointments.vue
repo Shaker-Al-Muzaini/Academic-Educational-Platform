7da407f485b41afa9bf17db64ffda6dc89c160c8
1shaker <salmazini@students.iugaza.edu.ps>
2023-08-25 00:26:10 +0300

    Editing3

--- resources/js/pages/appointments/ListAppointments.vue
+++ resources/js/pages/appointments/ListAppointments.vue
@@ -3,15 +3,82 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Appointments Page</h1>
+
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Appointments </li>
+                        <li class="breadcrumb-item active">Appointments</li>
                     </ol>
                 </div>
             </div>
         </div>
     </div>
-</template>
+
+
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="d-flex justify-content-between mb-2">
+                        <div>
+                            <router-link to="#">
+                                <button class="btn btn-primary"><i class="fa fa-plus-circle mr-1"></i> Add New
+                                    Appointment</button>
+                            </router-link>
+                        </div>
+                        <div class="btn-group btn-secondary">
+                            <button  type="button" class="btn"
+                                   >
+                                <span class="mr-1">All</span>
+                                <span class="badge badge-pill badge-info">10</span>
+                            </button>
+
+                            <button  type="button"
+                                    class="btn btn-secondary">
+                                <span class="mr-1">sd</span>
+                                <span class="badge badge-pill">12</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Client Name</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Time</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Options</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>test last</td>
+                                    <td>04:02</td>
+                                    <td>01:08</td>
+                                    <td>
+                                            <span class="badge-info">
+                                                on</span>
+                                    </td>
+                                    <td>
+                                        <router-link :to="'#'">
+                                            <i class="fa fa-edit mr-2"></i>
+                                        </router-link>
+
+                                        <a href="#">
+                                            <i class="fa fa-trash text-danger"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div></template>
