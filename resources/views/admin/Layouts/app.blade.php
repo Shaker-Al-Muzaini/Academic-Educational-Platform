1dfe6a56a1c7aa979ac898f21dfeb16327518658
1shaker <salmazini@students.iugaza.edu.ps>
2023-08-27 23:24:10 +0300

    Editing_end

--- resources/views/admin/Layouts/app.blade.php
+++ resources/views/admin/Layouts/app.blade.php
@@ -9,25 +9,6 @@
 
 <body class="hold-transition sidebar-mini">
 <div id="app"></div>
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
-        const toggleMenuIcon = document.getElementById('toggleMenuIcon');
-        const body = document.querySelector('body');
-
-        toggleMenuIcon.addEventListener('click', () => {
-            if (body.classList.contains('sidebar-collapse')) {
-                localStorage.setItem('sidebarState', 'expanded');
-            } else {
-                localStorage.setItem('sidebarState', 'collapsed');
-            }
-        });
-
-        const sidebarState = localStorage.getItem('sidebarState');
-        if (sidebarState === 'collapsed') {
-            body.classList.add('sidebar-collapse');
-        }
-    });
-</script>
 </body>
 
 </html>
