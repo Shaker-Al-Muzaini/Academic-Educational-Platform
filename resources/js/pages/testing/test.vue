ef230bf2a9bd076c2a0338e8e0c270d2ad67eabd
1shaker <salmazini@students.iugaza.edu.ps>
2023-09-18 18:55:55 +0300

    Add[7]

---
+++ resources/js/pages/testing/test.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Button from 'primevue/button';
+</script>
+
+<template>
+    <div class="card flex justify-content-center">
+        <Button label="Submit" />
+    </div>
+</template>
